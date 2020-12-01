ef39ea3b333816ade5fdd7ea6933a8d1985f5a6c
Edmundo Vitor <edmundovitor1414@hotmail.com>
2020-12-01 18:15:06 -0300

    Implementado BaseDAO

---
+++ src/model/DAO/BaseDAO.java
@@ -0,0 +1,26 @@
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+	Connection conn = null;
+	String url = "jdbc:postgresql://localhost:5432/ProjetoFinal";
+	String user = "postgres";
+	String senha = "bancodedados";
+	
+	public Connection getConnection() {
+		if(conn == null) {
+			try {
+				conn = DriverManager.getConnection(url, user, senha);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return conn;
+		}
+		else return conn;
+	}
+}
